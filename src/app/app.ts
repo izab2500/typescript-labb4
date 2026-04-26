ff0bce2016cd57b5ae9a5d5f34ea2c6ece9d5728
ziba <ziba2500@student.miun.se>
2026-04-26 16:47:34 +0200

    Load courses and signal in App-component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { CourseService } from './services/course-service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,7 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('TypeScript | labb4');
+  constructor(private coursesService: CourseService) { 
+    this.coursesService.loadCourses()
+  }
 }
