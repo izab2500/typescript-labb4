dad6ab890cddfc2ff62cee0e28f56ec78e8db4d8
ziba <ziba2500@student.miun.se>
2026-04-26 16:50:37 +0200

    Add heading-component

---
+++ src/app/heading/heading.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heading',
+  imports: [],
+  templateUrl: './heading.html',
+  styleUrl: './heading.css',
+})
+export class Heading {}
